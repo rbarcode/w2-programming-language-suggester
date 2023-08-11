55d82de9571016c66a13038cf86b4b0c42881b13
Richard Barbour <richyb07@gmail.com>
2023-08-11 14:52:35 -0700

    Add functionality to enter user name and display the name with any of the results.

--- js/scripts.js
+++ js/scripts.js
@@ -34,19 +34,14 @@ function hideResults() {
   document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("algorithm").setAttribute("class", "hidden");
 
-  // clearUserName();
-
   document.getElementById("submit-button").disabled = false;
 }
 
+/*NOTE: I'm confident there's a way to write this function better using a single class and collections or arrays and a for or foreach loop. I spent some time researching the topic but decided to use the method below because my research was taking too long. Thus, my commit history may not fully reflect the amount of time I have been working on this issue.*/
 function getInputName() {
   const inputName = document.getElementById("input-name").value;
-  const userName = document.querySelectorAll(".user-name");
-  for (let i = 0; i < userName.length; i++) {
-      userName.innerText = inputName;
-  }
+  document.getElementById("c#-user-name").innerText = inputName;
+  document.getElementById("c++-user-name").innerText = inputName;
+  document.getElementById("python-user-name").innerText = inputName;
+  document.getElementById("algorithm-user-name").innerText = inputName;
 }
-
-// function clearUserName() {
-//   document.getElementsByClassName("user-name").innerText = null;
-// }
